7bf6a62844c47fa5cffd3ab79c0381842d1fa57b
Paulo Uilian <uilianlbarros@gmail.com>
2021-06-28 17:55:33 -0300

    modificado estilo da cobra e retirado erros de jogabilidade

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
-let context = canvas.getContext("2d"); //....
-let widthCanva = 16;
-let heightCanva = 16;
-let tempo = 110;
-let box = 32;
+let context = canvas.getContext("2d"); //....;
+let widthCanva = 21;
+let heightCanva = 21;
+let tempo = 100;
+let box = 30;
 let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
 snake[0] ={
     x: 0 * box,
     y: 0 * box
 }
 
+let direction_cobra = [];
+direction_cobra[0] ={z: 1}
 
 
 let direction = "right";
+
 let food ={
     x: Math.floor(Math.random() * (widthCanva-1) + 1) * box,
     y: Math.floor(Math.random() * (heightCanva-1) + 1) * box
@@ -21,20 +24,43 @@ let food ={
 
 
 var img = new Image();
-img.src = 'img/fundo.jpg';
+img.src = 'img/fundo.png';
 function criarBG() {
   var pattern = context.createPattern(img, 'repeat');
   context.fillStyle = pattern;
   context.fillRect(0, 0, widthCanva*box, heightCanva*box);
 };
 
-function criarCobrinha (){
+
+
+function criarCobrinha() {
+    var img_cobra1 = new Image();
+    var img_cobra2 = new Image();
+    img_cobra1.src = 'img/c2.png';
+    img_cobra2.src = 'img/c1.png';
+
+    var corpo_cobra1 = context.createPattern(img_cobra1, 'repeat');
+    var corpo_cobra2 = context.createPattern(img_cobra2, 'repeat');
+
     for(i = 0; i < snake.length; i++){
-        context.fillStyle = "green";
+        
+        if (direction_cobra[i] == 1) {
+            context.fillStyle = corpo_cobra1;
+        } else {
+            context.fillStyle = corpo_cobra2;
+        }
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-
+/*
+function criarCobrinha (){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        if(i == 0) context.fillStyle = "purple";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+*/
 
 
 function drawFood (){
@@ -58,11 +84,7 @@ function update(event){
 
 
 function iniciarJogo(){    
-
-    if(snake[0].x > (widthCanva-1)*box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == 'left') snake[0].x = widthCanva * box;
-    if(snake[0].y > (heightCanva-1)*box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == 'up') snake[0].y = heightCanva * box;
+    
     
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
@@ -77,14 +99,31 @@ function iniciarJogo(){
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
+    let Z = 0;
 
-    if(direction == "right") snakeX += box;
-    if(direction == "left") snakeX -= box;
-    if (direction == "up") snakeY -= box;
-    if(direction == "down") snakeY += box;
+    if(direction == "right") {
+        snakeX += box;
+        Z = 1;
+    }
+
+    if(direction == "left") {
+        snakeX -= box;
+        Z = 1;
+    }
+
+    if (direction == "up") {
+        snakeY -= box;
+        Z = 0;
+    }
+
+    if(direction == "down") {
+        snakeY += box;
+        Z = 0;
+    };
 
     if(snakeX != food.x || snakeY != food.y){
         snake.pop(); //pop tira o último elemento da lista
+        direction_cobra.pop();
     }else{
         food.x = Math.floor(Math.random() * (widthCanva-1) +1) * box;
         food.y = Math.floor(Math.random() * (heightCanva-1) +1) * box;
@@ -95,7 +134,14 @@ function iniciarJogo(){
         y: snakeY
     }
 
+
     snake.unshift(newHead); //método unshift adiciona como primeiro quadradinho da cobrinha
+    direction_cobra.unshift(Z);
+
+    if(snake[0].x > (widthCanva-1)*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = (widthCanva - 1) * box;
+    if(snake[0].y > (heightCanva-1)*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = (heightCanva - 1) * box;
 }
 
 let jogo = setInterval(iniciarJogo, tempo);
